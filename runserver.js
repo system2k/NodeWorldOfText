4bc1108b399e4d9188498fb30baf1e0f453fd9d5
system2k <bsodtime100@gmail.com>
2017-09-17 15:50:47 -0400

    User prompt and other things

--- runserver.js
+++ runserver.js
@@ -21,7 +21,7 @@ const db = {
     // gets data from the database (only 1 row at a time)
     get: async function(command, params) {
         if(!params) params = []
-        return new Promise(function(r) {
+        return new Promise(function(r, rej) {
             database.get(command, params, function(err, res) {
                 if(err) {
                     return rej(false)
@@ -102,6 +102,7 @@ const db = {
         // table to inform that the server is initialized
         await db.run("CREATE TABLE 'server_info' (name TEXT, value TEXT)");
     }
+    var init = false;
     if(!await db.get("SELECT value FROM server_info WHERE name='initialized'")) {
         // server is not initialized
         console.log("Initializing server...");
@@ -112,8 +113,13 @@ const db = {
 
         await db.exec(tables)
         await db.exec(indexes)
+
+        init = true;
+        account_prompt();
     }
-    start_server();
+    if(!init) {
+        start_server();
+    }
 })()
 
 prompt.message      = ""; // do not display "prompt" before each question
@@ -162,9 +168,96 @@ const log_error = function(err) {
 	}
 }
 
+var pw_encryption = "sha512WithRSAEncryption";
+const encryptHash = function(pass, salt) {
+	if(!salt) {
+		var salt = crypto.randomBytes(10).toString("hex")
+	}
+	var hsh = crypto.createHmac(pw_encryption, salt).update(pass).digest("hex")
+	var hash = pw_encryption + "$" + salt + "$" + hsh;
+	return hash;
+};
 
+const checkHash = function(hash, pass) {
+	if(typeof hash !== "string") return false;
+	hash = hash.split("$");
+	if(hash.length !== 3) return false;
+	if(typeof pass !== "string") return false;
+	return encryptHash(pass, hash[1]) === hash.join("$");
+};
+
+// just to make things easier
+function toUpper(x) {
+    return x.toString().toUpperCase();
+}
+
+function account_prompt() {
+    passFunc = function(err, result) {
+		var err = false;
+		if(result['password'] !== result['confirmpw']) {
+			console.log("Error: Your passwords didn't match.")
+			err = true;
+			prompt.get(prompt_account_properties, passFunc);
+		} else if(result.password.length > 128) {
+			console.log("The password is too long. It must be 128 characters or less.");
+			err = true;
+			prompt.get(prompt_account_properties, passFunc);
+		}
+
+		if(result.username.length > 30) {
+			console.log("The username must be 30 characters or less.")
+			err = true;
+			prompt.get(prompt_account_properties, passFunc);
+		} else if(result.username.length < 1) {
+			console.log("The username is too short");
+			err = true;
+			prompt.get(prompt_account_properties, passFunc);
+		} else if(!result.username.match(/^(\w*)$/g)) {
+			console.log("The username must contain the following characters: a-z A-Z 0-9 _");
+			err = true;
+			prompt.get(prompt_account_properties, passFunc);
+		}
+		
+		if(!err){
+			var Date_ = Date.now()
+            var passHash = encryptHash(result['password'])
+
+            db.run("INSERT INTO auth_user VALUES(null, ?, '', '', '', ?, 1, 1, 1, ?, ?)",
+                [result["username"], passHash, Date_, Date_])
+
+            console.log("Superuser created successfully.\n");
+            start_server();
+		}
+	}
+	yesNoAccount = function(err, result) {
+		var re = result['yes_no_account'];
+		if(toUpper(re) === "YES") {
+			prompt.get(prompt_account_properties, passFunc);
+		}
+		if(toUpper(re) === "NO") {
+			start_server()
+		}
+		if(toUpper(re) !== "YES" && toUpper(re) !== "NO") {
+			console.log("Please enter either \"yes\" or \"no\" (not case sensitive):");
+			prompt.get(prompt_account_yesno, yesNoAccount);
+		}
+    }
+    prompt.start();
+    prompt.get(prompt_account_yesno, yesNoAccount);
+}
+
+//Time in milliseconds
+var Second = 1000;
+var Minute = 60000;
+var Hour = 3600000;
+var Day = 86400000;
+var Week = 604800000;
+var Month = 2628002880;
+var Year = 31536034560;
+var Decade = 315360345600;
 
 var server = http.createServer(function(req, res) {
+    // use this if you do not want the request data to be cached
     /*res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
     res.setHeader("Pragma", "no-cache");
     res.setHeader("Expires", "0");*/
@@ -174,6 +267,11 @@ var server = http.createServer(function(req, res) {
     res.end("TEST: " + URL);
 })
 function start_server() {
+    (async function clear_expired_sessions() {
+        await db.run("DELETE FROM auth_session WHERE expire_date <= ?", Date.now());
+        setTimeout(clear_expired_sessions, Minute);
+    })()
+
     server.listen(settings.port, function() {
         var addr = server.address();
         console.log("Server is running.\nAddress: " + addr.address + "\nPort: " + addr.port);
