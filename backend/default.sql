0224a161e5cea2f72ad62e05b1177c01430f7e52
system2k <bsodtime100@gmail.com>
2017-09-17 15:13:20 -0400

    Changes

---
+++ backend/default.sql
@@ -0,0 +1,69 @@
+PRAGMA encoding='UTF-16';
+
+CREATE TABLE "auth_user" (
+    "id" integer NOT NULL PRIMARY KEY,
+    "username" varchar(30) NOT NULL UNIQUE,
+    "first_name" varchar(30) NOT NULL,
+    "last_name" varchar(30) NOT NULL,
+    "email" varchar(75) NOT NULL,
+    "password" varchar(128) NOT NULL,
+    "is_staff" bool NOT NULL,
+    "is_active" bool NOT NULL,
+    "is_superuser" bool NOT NULL,
+    "last_login" integer NOT NULL,
+    "date_joined" integer NOT NULL
+);
+
+CREATE TABLE "auth_session" (
+    "session_key" varchar(40) NOT NULL PRIMARY KEY,
+    "session_data" text NOT NULL,
+    "expire_date" integer NOT NULL
+);
+
+CREATE TABLE "registration_registrationprofile" (
+    "id" integer NOT NULL PRIMARY KEY,
+    "user_id" integer NOT NULL UNIQUE REFERENCES "auth_user" ("id"),
+    "activation_key" varchar(40) NOT NULL
+);
+
+CREATE TABLE "edit" (
+    "id" integer NOT NULL PRIMARY KEY,
+    "user_id" integer REFERENCES "auth_user" ("id"),
+    "ip" char(15),
+    "world_id" integer NOT NULL REFERENCES "world" ("id"),
+    "tileY" integer NOT NULL,
+    "tileX" integer NOT NULL,
+    "time" integer NOT NULL,
+    "content" text NOT NULL
+);
+
+CREATE TABLE "tile" (
+    "id" integer NOT NULL PRIMARY KEY,
+    "world_id" integer NOT NULL REFERENCES "world" ("id"),
+    "content" varchar(128) NOT NULL,
+    "tileY" integer NOT NULL,
+    "tileX" integer NOT NULL,
+    "properties" text NOT NULL,
+    "created_at" integer NOT NULL,
+    UNIQUE ("world_id", "tileY", "tileX")
+);
+
+CREATE TABLE "whitelist" (
+    "id" integer NOT NULL PRIMARY KEY,
+    "user_id" integer NOT NULL REFERENCES "auth_user" ("id"),
+    "world_id" integer NOT NULL REFERENCES "world" ("id"),
+    "created_at" integer NOT NULL,
+    "updated_at" integer NOT NULL,
+    UNIQUE ("user_id", "world_id")
+);
+
+CREATE TABLE "world" (
+    "id" integer NOT NULL PRIMARY KEY,
+    "name" text NOT NULL UNIQUE,
+    "owner_id" integer REFERENCES "auth_user" ("id"),
+    "created_at" integer NOT NULL,
+    "updated_at" integer NOT NULL,
+    "public_readable" bool NOT NULL,
+    "public_writable" bool NOT NULL,
+    "properties" text NOT NULL
+);
--- runserver.js
+++ runserver.js
@@ -1,13 +1,168 @@
-const http = require("http");
-const url = require("url");
-const sql = require("sqlite3").verbose();
-const fs = require("fs");
-const swig = require("swig");
-const querystring = require("querystring");
-const crypto = require("crypto");
+const http          = require("http");
+const url           = require("url");
+const sql           = require("sqlite3").verbose();
+const fs            = require("fs");
+const swig          = require("swig");
+const querystring   = require("querystring");
+const crypto        = require("crypto");
+const mime          = require("./backend/mime.js");
+const prompt        = require("./backend/prompt.js");
 
 const settings = require("./settings.json");
-console.log(settings)
+const database = new sql.Database(settings.DATABASE_PATH);
+
+var static_path = "./program/html/static/";
+var template_path = "./program/html/templates/";
+
+var sql_table_init = "./backend/default.sql";
+var sql_indexes_init = "./backend/indexes.sql";
+
+const db = {
+    // gets data from the database (only 1 row at a time)
+    get: async function(command, params) {
+        if(!params) params = []
+        return new Promise(function(r) {
+            database.get(command, params, function(err, res) {
+                if(err) {
+                    return rej(false)
+                }
+                r(res)
+            })
+        })
+    },
+    // runs a command (insert, update, etc...) and might return "lastID" if needed
+    run: async function(command, params) {
+        if(!params) params = []
+        var err = false
+        return new Promise(function(r, rej) {
+            database.run(command, params, function(err, res) {
+                if(err) {
+                    return rej(err)
+                }
+                var info = {
+                    lastID: this.lastID
+                }
+                r(info)
+            })
+        })
+    },
+    // gets multiple rows in one command
+    all: async function(command, params) {
+        if(!params) params = []
+        return new Promise(function(r, rej) {
+            database.all(command, params, function(err, res) {
+                if(err) {
+                    return rej(err)
+                }
+                r(res)
+            })
+        })
+    },
+    // get multiple rows but execute a function for every row
+    each: async function(command, params, callbacks) {
+        if(typeof params == "function") {
+            callbacks = params
+            params = []
+        }
+        var def = callbacks
+        var callback_error = false
+        callbacks = function() {
+            try {
+                def(...arguments)
+            } catch(e) {
+                callback_error = true
+            }
+        }
+        return new Promise(function(r, rej) {
+            database.each(command, params, callbacks, function(err, res) {
+                if(err || callback_error) {
+                    return rej(err)
+                }
+                r(res)
+            })
+        })
+    },
+    // like run, but executes the command as a SQL file
+    // (no comments allowed, and must be semicolon seperated)
+    exec: async function(command) {
+        return new Promise(function(r, rej) {
+            database.exec(command, function(err) {
+                if(err) {
+                    return rej(err)
+                }
+                r(true)
+            })
+        })
+    }
+};
+
+(async function() {
+    console.log("Starting server...");
+    if(!await db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='server_info'")) {
+        // table to inform that the server is initialized
+        await db.run("CREATE TABLE 'server_info' (name TEXT, value TEXT)");
+    }
+    if(!await db.get("SELECT value FROM server_info WHERE name='initialized'")) {
+        // server is not initialized
+        console.log("Initializing server...");
+        await db.run("INSERT INTO server_info VALUES('initialized', 'true')");
+
+        var tables = fs.readFileSync(sql_table_init).toString();
+        var indexes = fs.readFileSync(sql_indexes_init).toString();
+
+        await db.exec(tables)
+        await db.exec(indexes)
+    }
+    start_server();
+})()
+
+prompt.message      = ""; // do not display "prompt" before each question
+prompt.delimiter    = ""; // do not display ":" after "prompt"
+prompt.colors       = false; // disable dark gray color in a black console
+
+var prompt_account_properties = {
+	properties: {
+		username: {
+			message: 'Username: '
+		},
+		password: {
+			description: 'Password: ',
+			replace: '*',
+			hidden: true
+		},
+		confirmpw: {
+			description: 'Password (again): ',
+			replace: '*',
+			hidden: true
+		}
+	}
+};
+
+var prompt_account_yesno = {
+	properties: {
+		yes_no_account: {
+			message: "You just installed the server, which means you don\'t have any superusers defined.\nWould you like to create one now? (yes/no):"
+		}
+	}
+};
+
+const log_error = function(err) {
+	if(settings.error_log) {
+		try {
+			var errs = err;
+			if(typeof errs !== "string") {
+				errs = errs.stack
+			}
+			errs = JSON.stringify(errs);
+			err = "[" + errs + ", " + Date.now() + "]\r\n";
+			fs.appendFile(settings.LOG_PATH, err);
+		} catch(e) {
+			console.log(e)
+		}
+	}
+}
+
+
 
 var server = http.createServer(function(req, res) {
     /*res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
@@ -18,7 +173,11 @@ var server = http.createServer(function(req, res) {
 
     res.end("TEST: " + URL);
 })
-server.listen(80, function() {
-    var addr = server.address();
-    console.log("SERVER: " + addr.address + ":" + addr.port)
-});
+function start_server() {
+    server.listen(settings.port, function() {
+        var addr = server.address();
+        console.log("Server is running.\nAddress: " + addr.address + "\nPort: " + addr.port);
+    });
+}
+
+// https thing: https://gist.github.com/davestevens/c9e437afbb41c1d5c3ab
