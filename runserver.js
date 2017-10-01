46318e4cfcf223cc5e2b82b40a5c972dd856322a
system2k <bsodtime100@gmail.com>
2017-09-30 20:23:06 -0400

    fix git conflict... wtf

--- runserver.js
+++ runserver.js
@@ -1004,7 +1004,7 @@ function start_server() {
         setTimeout(clear_expired_sessions, Minute);
     })();
 
-    server.listen(process.env.PORT || 5000, function() {
+    server.listen(settings.port, function() {
         var addr = server.address();
         console.log("Server is running.\nAddress: " + addr.address + "\nPort: " + addr.port);
 
