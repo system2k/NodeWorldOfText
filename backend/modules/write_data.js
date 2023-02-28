0f979bfa1f3ed785054c8574963dd68b0f8ec97f
system2k <bsodtime100@gmail.com>
2023-02-27 20:37:53 -0500

    Rewrite edit organizer

--- backend/modules/write_data.js
+++ backend/modules/write_data.js
@@ -13,6 +13,32 @@ function isMainPage(name) {
 	return name == "" || name.toLowerCase() == "main" || name.toLowerCase() == "owot";
 }
 
+function partitionMultiEdit(editOffset, tileX, tileY, char, color, bgColor, editId) {
+	var res = [];
+	for(var i = 0; i < char.length; i++) {
+		var subChar = char[i];
+		var subColor = color;
+		var subBgColor = bgColor;
+		if(Array.isArray(color)) subColor = color[i];
+		if(Array.isArray(bgColor)) subBgColor = bgColor[i];
+		
+		var offset = editOffset + i;
+		if(offset < 0 || offset >= CONST.tileArea) continue;
+
+		var charY = Math.floor(offset / CONST.tileCols);
+		var charX = offset % CONST.tileCols;
+
+		subColor = sanitize_color(subColor);
+		if(subBgColor !== null) {
+			if(subBgColor === void 0) subBgColor = -1;
+			subBgColor = sanitize_color(subBgColor);
+		}
+
+		res.push([tileY, tileX, charY, charX, 0, subChar, editId, subColor, subBgColor]);
+	}
+	return res;
+}
+
 /*
 REFACTOR:
 The job of this module should be to take a properly-sanitized array of edits
@@ -117,12 +143,28 @@ module.exports = async function(data, server, params) {
 	var totalEdits = 0;
 	var tiles = {};
 	var tileCount = 0;
-	// organize edits into tile coordinates
+
+	var validEdits = [];
+
 	for(var i = 0; i < edits.length; i++) {
-		var segment = edits[i];
-		if(!segment || !Array.isArray(segment)) continue;
-		var tileY = san_nbr(segment[0]);
-		var tileX = san_nbr(segment[1]);
+		var edit = edits[i];
+		if(!edit || !Array.isArray(edit)) continue;
+		
+		var tileY = san_nbr(edit[0]);
+		var tileX = san_nbr(edit[1]);
+		var charY = san_nbr(edit[2]);
+		var charX = san_nbr(edit[3]);
+		var date = edit[4];
+		var char = edit[5];
+		var editId = san_nbr(edit[6]);
+		var color = edit[7];
+		var bgColor = edit[8];
+
+		if(typeof char != "string") continue;
+
+		var editOffset = charY * CONST.tileCols + charX;
+		if(editOffset < 0 || editOffset >= CONST.tileArea) continue;
+
 		var charRatePerSecond = defaultCharRatePerSecond;
 
 		var rrate = rate_limiter.checkCharrateRestr(restr, ipAddressVal, ipAddressFam, isGrouped, world.name, tileX, tileY);
@@ -130,44 +172,68 @@ module.exports = async function(data, server, params) {
 			charRatePerSecond = rrate;
 		}
 
-		var tileStr = tileY + "," + tileX;
-		var char = segment[5];
-		segment[6] = san_nbr(segment[6]); // edit id
-		var editID = segment[6];
-		if(typeof char != "string") continue;
 		if(!rate_limiter.checkCharRateLimit(editLimiter, charRatePerSecond, 1)) {
-			rejected[editID] = 2;
-			if(charRatePerSecond == 0) rejected[editID] = 4;
+			rejected[editId] = 2;
+			if(charRatePerSecond == 0) rejected[editId] = 4;
 			continue;
 		}
 		if(customLimiter && rrate == null) {
 			if(!rate_limiter.checkCharRateLimit(customLimiter, charsPerPeriod, 1)) {
-				rejected[editID] = 2;
-				if(charsPerPeriod == 0) rejected[editID] = 4;
+				rejected[editId] = 2;
+				if(charsPerPeriod == 0) rejected[editId] = 4;
 				continue;
 			}
 		}
 		if(isHTTP && httpWriteDenied) {
-			rejected[editID] = 4;
+			rejected[editId] = 4;
 			continue;
 		}
+
+		totalEdits++;
+		if(totalEdits > editLimit) { // edit limit reached
+			break;
+		}
+
+		var tileStr = tileY + "," + tileX;
 		if(!tiles[tileStr]) {
 			if(!rate_limiter.checkTileRateLimit(tileLimiter, tileRatePerSecond, tileX, tileY, world_id)) {
-				rejected[editID] = 3;
+				rejected[editId] = 3;
 				continue;
 			}
 			if(!rate_limiter.setHold(idLabel, tileX, tileY)) {
-				rejected[editID] = 5;
+				rejected[editId] = 5;
 				continue;
 			}
 			tiles[tileStr] = [];
 			tileCount++;
 		}
-		totalEdits++;
-		if(totalEdits > editLimit) { // edit limit reached
-			break;
+
+		if(rate_limiter.checkColorRestr(restr, ipAddressVal, ipAddressFam, isGrouped, world.name, tileX, tileY)) {
+			color = 0;
+			bgColor = -1;
 		}
-		tiles[tileStr].push(segment);
+
+		var testChar = advancedSplit(char);
+		if(testChar.length == 0) {
+			char = "\0";
+		} else if(char.length > 1 && (user.superuser || is_member || is_owner)) {
+			validEdits.push(...partitionMultiEdit(editOffset, tileX, tileY, char, color, bgColor, editId));
+			continue;
+		}
+
+		charY = Math.floor(editOffset / CONST.tileCols);
+		charX = editOffset % CONST.tileCols;
+
+		if(Array.isArray(color)) color = color[0];
+		if(Array.isArray(bgColor)) bgColor = bgColor[0];
+
+		color = sanitize_color(color);
+		if(bgColor !== null) {
+			if(bgColor === void 0) bgColor = -1;
+			bgColor = sanitize_color(bgColor);
+		}
+
+		validEdits.push([tileY, tileX, charY, charX, 0, char, editId, color, bgColor]);
 	}
 
 	if(params && monitorEventSockets.length) {
@@ -186,115 +252,8 @@ module.exports = async function(data, server, params) {
 	}
 
 	var currentDate = Date.now();
-	var tile_edits = [];
 
-	for(var i in tiles) {
-		var incomingEdits = tiles[i];
-		var changes = [];
-
-		var canColor = true;
-		var pos = i.split(",");
-		var tileX = parseInt(pos[1]);
-		var tileY = parseInt(pos[0]);
-		if(rate_limiter.checkColorRestr(restr, ipAddressVal, ipAddressFam, isGrouped, world.name, tileX, tileY)) {
-			canColor = false;
-		}
-
-		for(var k = 0; k < incomingEdits.length; k++) {
-			var editIncome = incomingEdits[k];
-
-			editIncome[0] = san_nbr(editIncome[0]);
-			editIncome[1] = san_nbr(editIncome[1]);
-			var charX = san_nbr(editIncome[3]);
-			var charY = san_nbr(editIncome[2]);
-			var charInsIdx = charY * CONST.tileCols + charX;
-			if(charInsIdx < 0) charInsIdx = 0;
-			if(charInsIdx > CONST.tileArea - 1) charInsIdx = CONST.tileArea - 1;
-
-			charX = charInsIdx % CONST.tileCols;
-			charY = Math.floor(charInsIdx / CONST.tileCols);
-			editIncome[3] = charX;
-			editIncome[2] = charY;
-
-			var char = editIncome[5];
-			if(typeof char != "string") {
-				char = "?";
-			}
-			char = advancedSplit(char);
-			if(char.length <= 1) {
-				if(!editIncome[7]) editIncome[7] = 0;
-				if(Array.isArray(editIncome[7])) {
-					editIncome[7] = sanitize_color(editIncome[7][0]);
-				} else {
-					editIncome[7] = sanitize_color(editIncome[7]);
-				}
-				// client is restricted from using colors at specific parameters
-				if(!canColor) {
-					editIncome[7] = 0;
-				}
-
-				// validate & normalize the bgColor (no guarantee it will be accepted)
-				var bgColor = editIncome[8];
-				if(bgColor !== null) {
-					if(bgColor === void 0) bgColor = -1;
-					bgColor = san_nbr(bgColor);
-					if(bgColor < -1) bgColor = -1;
-					if(bgColor > 16777215) bgColor = 16777215;
-				}
-				editIncome[8] = bgColor;
-
-				changes.push(editIncome);
-				continue;
-			} else {
-				// only password holders, superusers, owners, or members can use multiple characters per edit
-				if(!user.superuser && !(is_owner || is_member)) {
-					char = char.slice(0, 1);
-				}
-			}
-			// TODO: refactor this bit of redundant code
-			for(var i = 0; i < char.length; i++) {
-				var newIdx = charInsIdx + i;
-				if(newIdx > CONST.tileArea - 1) continue; // overflow
-				// convert back to proper X/Y
-				var newX = newIdx % CONST.tileCols;
-				var newY = Math.floor(newIdx / CONST.tileCols);
-				var newChar = char[i]; // don't filter the edit yet. that is handled in the database subsystem.
-				var newColor = editIncome[7];
-				var newBgColor = editIncome[8];
-				if(Array.isArray(newColor)) {
-					// color is an array, get individual values
-					newColor = sanitize_color(newColor[i]);
-				} else {
-					// color is a number
-					newColor = sanitize_color(newColor);
-				}
-				if(!newColor) newColor = 0;
-
-				if(Array.isArray(newBgColor)) {
-					newBgColor = newBgColor[i];
-				}
-
-				if(newBgColor !== null) {
-					if(newBgColor === void 0) newBgColor = -1;
-					newBgColor = san_nbr(newBgColor);
-					if(newBgColor < -1) newBgColor = -1;
-					if(newBgColor > 16777215) newBgColor = 16777215;
-				}
-
-				var newAr = [editIncome[0], editIncome[1],
-							newY, newX,
-							editIncome[4], newChar, editIncome[6], newColor, newBgColor];
-				changes.push(newAr);
-			}
-		}
-
-		for(var e = 0; e < changes.length; e++) {
-			var change = changes[e];
-			tile_edits.push(change);
-		}
-	}
-
-	if(!tile_edits.length) {
+	if(!validEdits.length) {
 		rate_limiter.clearHolds(idLabel, tiles);
 		return {
 			accepted: [],
@@ -305,7 +264,7 @@ module.exports = async function(data, server, params) {
 	// send to tile database manager
 	var resp = await tile_database.write(tile_database.types.write, {
 		date: currentDate,
-		tile_edits,
+		tile_edits: validEdits,
 		user, world, is_owner, is_member,
 		can_color_text, can_color_cell,
 		public_only, no_log_edits, preserve_links,
