755024a747ccac89068ef5e43767beb0c274f762
system2k <bsodtime100@gmail.com>
2023-12-15 22:42:30 -0500

    Remove redundant if-checks in drawShadeChar

--- frontend/static/yw/javascript/renderer.js
+++ frontend/static/yw/javascript/renderer.js
@@ -520,29 +520,25 @@ function isValidSpecialSymbol(charCode) {
 }
 
 function drawShadeChar(charCode, textRender, x, y, clampW, clampH, flags) {
-	if(charCode >= 0x2591 && charCode <= 0x2593) {
-		let isLight = charCode == 0x2591;
-		let factor = isLight ? 3 : 5;
-		textRender.beginPath();
-		for(let i = 0; i < factor; i++) {
-			for(let j = 0; j < 10; j++) {
-				textRender.rect(
-					x + (i * clampW / factor) + (!(j%2)) * (clampW / (factor * 2)),
-					y + j * clampH/ 10,
-					clampW / 10,
-					clampH / 20);
-			}
+	let isLight = charCode == 0x2591;
+	let factor = isLight ? 3 : 5;
+	textRender.beginPath();
+	for(let i = 0; i < factor; i++) {
+		for(let j = 0; j < 10; j++) {
+			textRender.rect(
+				x + (i * clampW / factor) + (!(j%2)) * (clampW / (factor * 2)),
+				y + j * clampH/ 10,
+				clampW / 10,
+				clampH / 20);
 		}
-		if(charCode == 0x2593) {
-			for(let j = 0; j < 10; j++){
-				textRender.rect(x, y + clampH / 20 + j * clampH / 10,
-								clampW, clampH / 20);
-			}
-		}
-		textRender.fill();
-		return true;
 	}
-	return false;
+	if(charCode == 0x2593) {
+		for(let j = 0; j < 10; j++) {
+			textRender.rect(x, y + clampH / 20 + j * clampH / 10,
+				clampW, clampH / 20);
+		}
+	}
+	textRender.fill();
 }
 
 function draw2by2Char(charCode, textRender, x, y, width, height) {
